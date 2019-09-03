b1cacdca40ba9af45fa0584cb9d1be72d5b3cec8
PassaUmDollar <46573030+PassaUmDollar@users.noreply.github.com>
2019-09-03 14:33:29 -0300

    Fix

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,19 @@
 var app = new Vue({
     el: "#app",
     data: {
-        links: [{"name" : "Inicio", "ativo" : true, "acessou": true},{"name" : "Contato", "ativo" : false, "acessou": false}],
-        acessei: {"name" : "Inicio", "ativo" : true, "acessou": true}
+        links: [{"name" : "Inicio", "ativo" : true, "acessou": true}
+        ,{"name" : "Contato", "ativo" : false, "acessou": false},
+        {"name" : "Criar 3D", "ativo" : false, "acessou": false}
+        ],
+        acessei: {"name" : "Inicio", "ativo" : true, "acessou": true},
+        produtos: [
+            {"name" : "Faca Adaga das Sombras CS:GO", "preco" : 20, "img" : "./produtos/1.jpg"},
+            {"name" : "Suporte de tomada para celular", "preco" : 15 , "img" : "./produtos/2.jpg"},
+            {"name" : "Polvo", "preco" : 30 , "img" : "./produtos/3.jpg"},
+            {"name" : "Dinossauro Flexível", "preco" : 20 , "img" : "./produtos/4.jpg"}
+        ],
+        view: false,
+        vendo: {}
     },
     methods: {
        acessarLink(link){
@@ -16,6 +27,13 @@ var app = new Vue({
             this.links[index] = link
             this.acessei = link
             return false
+       },
+       viewImage(image){
+        this.view = true
+        this.vendo = image
+       },
+       close(){
+           this.view = false
        }
     }
 })
